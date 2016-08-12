1bbcfd5c65826553dda1e96efebe2206d989d6ea
larryhudson <harryludson@gmail.com>
2016-08-12 22:09:15 +1000

    Added Sheet2 - shows the average price for each year and how many cars were counted for each year

--- carsales_calc.py
+++ carsales_calc.py
@@ -6,8 +6,15 @@ import os.path
 import numpy
 driver = webdriver.Firefox()
 
-user_make = "Toyota"
-user_model = "Corolla"
+transmission_switch = True
+price_switch = True
+
+def get_average(list):
+	price_list = []
+	for item in list:
+		price = item[4]
+		price_list.append(price)
+	return numpy.average(price_list)
 
 # setup config file. if it exists, load it
 config = configparser.ConfigParser()
@@ -17,10 +24,12 @@ if os.path.isfile('carsales_calc.ini'):
 	user_state = user_settings["State"]
 	if "Transmission" in user_settings:
 		user_transmission = user_settings["Transmission"]
+	else:
+		transmission_switch = False
 	if "Min price" in user_settings:
 		price_min, price_max = user_settings["Min price"], user_settings["Max price"]
 	else:
-		price_string = ""
+		price_switch = False
 	kms_min, kms_max = user_settings["Min kms"], user_settings["Max kms"]
 else:
 	config.add_section("user")
@@ -52,12 +61,12 @@ else:
 		config.write(configfile)
 
 if transmission_switch:
-	transmission_string = "GenericGearType%3D%5B" + user_transmission
+	transmission_string = "%26GenericGearType%3D%5B" + user_transmission + "%5D%29"
 else:
 	transmission_string = ""
 
 if price_switch:	
-	price_string = "Price%3Drange%5B" + str(price_min) + ".." + str(price_max)
+	price_string = "%26Price%3Drange%5B" + str(price_min) + ".." + str(price_max) + "%5D%29"
 else:
 	price_string = ""		
 		
@@ -67,7 +76,7 @@ car_choice = input("> ")
 user_make, user_model = car_choice.split(" ")
 
 print("Searching Carsales")
-driver.get("http://www.carsales.com.au/cars/results?q=%28%28%28%28%28%28%28Make%3D%5B" + user_make + "%5D%26Model%3D%5B" + user_model + "%5D%29%26State%3D%5B" + user_state + "%5D%29%26Service%3D%5BCarsales%5D%29%26" + transmission_string + "%5D%29%26" + price_string + "%5D%29%26Odometer%3Drange%5B" + str(kms_min) + ".." + str(kms_max) + "%5D%29%26%28BodyStyle%3D%5BHatch%5D%7CBodyStyle%3D%5BSedan%5D%29%29&sortby=Year&limit=24")
+driver.get("http://www.carsales.com.au/cars/results?q=%28%28%28%28%28%28%28Make%3D%5B" + user_make + "%5D%26Model%3D%5B" + user_model + "%5D%29%26State%3D%5B" + user_state + "%5D%29%26Service%3D%5BCarsales%5D%29" + transmission_string + price_string + "%26Odometer%3Drange%5B" + str(kms_min) + ".." + str(kms_max) + "%5D%29%26%28BodyStyle%3D%5BHatch%5D%7CBodyStyle%3D%5BSedan%5D%29%29&sortby=Year&limit=24")
 
 num_search_results = int(driver.find_elements_by_css_selector("div.results-title")[0].text.split(' ')[0])
 
@@ -85,7 +94,7 @@ while current_page <= page_count:
 	page_listings = driver.find_elements_by_css_selector("div.listing-item")
 	real_page_listings = []
 	for car in page_listings:
-		if not "contentcards" in car.get_attribute('class'):
+		if not "contentcards" in car.get_attribute('class') and not "gcad" in car.get_attribute('class'):
 			real_page_listings.append(car)
 	for car in real_page_listings:
 		car_link = car.find_elements_by_tag_name('a')[0].get_attribute('href')
@@ -101,6 +110,17 @@ while current_page <= page_count:
 		driver.get(next_page_link)
 	current_page += 1
 
+by_year = []
+years_done = []
+for car in car_list:
+	year_list = []
+	for sublist in car_list:
+		if car[2] == sublist[2]:
+			year_list.append(sublist)
+	if not car[2] in years_done:
+		by_year.append(year_list)
+		years_done.append(car[2])
+		
 # link, title, year, kms, price
 now = datetime.datetime.now()
 now_string = now.strftime("%d-%m-%y %H.%M")
@@ -111,7 +131,6 @@ worksheet = workbook.add_worksheet()
 
 bold = workbook.add_format({'bold': 1})
 url_format = workbook.add_format({'font_color': 'blue','underline': 1})
-
 
 worksheet.write('A1', 'Link', bold)
 worksheet.write('B1', 'Year', bold)
@@ -127,5 +146,26 @@ for car in car_list:
 	worksheet.write_number(row, col + 1, car[2])
 	worksheet.write_number(row, col + 2, car[3])
 	worksheet.write_number(row, col + 3, car[4])
+
+	
+worksheet2 = workbook.add_worksheet()
+
+worksheet2.write('A1', 'Year', bold)
+worksheet2.write('B1', 'Average', bold)
+worksheet2.write('C1', 'Count', bold)
+
+row = 1
+col = 0
+
+for year in by_year:
+	row = by_year.index(year) + 1
+	average = get_average(year)
+	car_num = len(year)
+	worksheet2.write_number(row, col, year[0][2])
+	worksheet2.write_number(row, col + 1, average)
+	worksheet2.write_number(row, col + 2, car_num)
+	
+	
+	
 print("Saving spreadsheet")
 workbook.close()

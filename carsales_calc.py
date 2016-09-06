40d31e0e962075f75d7a4040fe4b5d76706f9ed9
Larry Hudson <harryludson@gmail.com>
2016-09-06 22:11:13 +1000

    Changed list indexing to namedtuple
    So instead of car[0]-car[4], it's car.link, car.kms etc. Makes
    the code more straightforward.

--- carsales_calc.py
+++ carsales_calc.py
@@ -4,6 +4,7 @@ import datetime
 import configparser
 import os.path
 import numpy
+from collections import namedtuple
 
 transmission_switch = True
 price_switch = True
@@ -102,6 +103,7 @@ def main():
         page_count = 1
     current_page = 1
     car_list = []
+    Car = namedtuple("Car", ['link', 'title', 'year', 'kms', 'price'])
 
     while current_page <= page_count:
         print("Loading page", str(current_page))
@@ -117,7 +119,8 @@ def main():
             car_year = int(car_title.split(' ')[0])
             car_price = int(car.find_elements_by_css_selector('div.price')[0].text.strip("$*").replace(",",""))
             car_kms = int(car.find_elements_by_css_selector('div.feature-text')[0].text.strip(" km").replace(",",""))
-            car_list.append([car_link, car_title, car_year, car_kms, car_price])
+            car_tuple = Car(car_link, car_title, car_year, car_kms, car_price)
+            car_list.append(car_tuple)
         if current_page < page_count:
             pagination_div = driver.find_elements_by_css_selector("div.pagination")[0]
             next_page_link = pagination_div.find_elements_by_partial_link_text("Next")[0].get_attribute('href')
@@ -156,10 +159,10 @@ def main():
 
     for car in car_list:
         row = car_list.index(car) + 1
-        worksheet.write_url(row, col, car[0], url_format, car[1])
-        worksheet.write_number(row, col + 1, car[2])
-        worksheet.write_number(row, col + 2, car[3])
-        worksheet.write_number(row, col + 3, car[4])
+        worksheet.write_url(row, col, car.link, url_format, car.title)
+        worksheet.write_number(row, col + 1, car.year)
+        worksheet.write_number(row, col + 2, car.kms)
+        worksheet.write_number(row, col + 3, car.price)
 
 
     worksheet2 = workbook.add_worksheet()
